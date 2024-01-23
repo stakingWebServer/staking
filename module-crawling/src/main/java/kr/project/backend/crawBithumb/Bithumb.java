672cc5df22244e6cb35e4b6f70f8239ca4dce218
yjkim-mtn <newy12@naver.com>
2024-01-23 13:29:31 +0900

    #feat. 크롤링 실제 배포

--- module-crawling/src/main/java/kr/project/backend/crawBithumb/Bithumb.java
+++ module-crawling/src/main/java/kr/project/backend/crawBithumb/Bithumb.java
@@ -43,7 +43,7 @@ public class Bithumb {
 
         //크롬드라이브 세팅
         System.setProperty("webdriver.chrome.driver", String.valueOf(ResourceUtils.getFile("/usr/bin/chromedriver")));
-        System.setProperty("webdriver.chrome.driver", String.valueOf(ResourceUtils.getFile("classpath:static/chromedriver")));
+        //System.setProperty("webdriver.chrome.driver", String.valueOf(ResourceUtils.getFile("classpath:static/chromedriver")));
         ChromeOptions options = new ChromeOptions();
         //배포할때 주석풀기.
         options.addArguments("headless","no-sandbox","disable-dev-shm-usage");
