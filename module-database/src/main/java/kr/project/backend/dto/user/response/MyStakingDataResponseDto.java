91b8ab527dddc42534c795e7cc58a09d580f9be5
namgihyeon <skarlgus0419@naver.com>
2024-02-01 00:44:27 +0900

    #feat. 마이데이터 리스트 응답값 추가

--- module-database/src/main/java/kr/project/backend/dto/user/response/MyStakingDataResponseDto.java
+++ module-database/src/main/java/kr/project/backend/dto/user/response/MyStakingDataResponseDto.java
@@ -15,6 +15,7 @@ public class MyStakingDataResponseDto implements Serializable {
     private String totalHoldings;
     private String totalRewards;
     private CoinMarketType coinMarketType;
+    private String unit;
 
     public MyStakingDataResponseDto(Favorite favorite) {
         DecimalFormat decimalFormat = new DecimalFormat("#.##################");
@@ -24,5 +25,6 @@ public class MyStakingDataResponseDto implements Serializable {
         this.totalHoldings = decimalFormat.format(favorite.getTotalHoldings());
         this.totalRewards = decimalFormat.format(favorite.getTotalRewards());
         this.coinMarketType = favorite.getStakingInfo().getCoinMarketType();
+        this.unit = favorite.getStakingInfo().getUnit();
     }
 }
