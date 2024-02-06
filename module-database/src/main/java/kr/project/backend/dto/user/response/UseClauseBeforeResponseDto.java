6168d2bc0996f02fe0d3d8b5ac7ea0865b875577
namgihyeon <skarlgus0419@naver.com>
2024-02-06 17:54:50 +0900

    #feat. 이전약관 조회시 약관구분코드 추가

--- module-database/src/main/java/kr/project/backend/dto/user/response/UseClauseBeforeResponseDto.java
+++ module-database/src/main/java/kr/project/backend/dto/user/response/UseClauseBeforeResponseDto.java
@@ -6,11 +6,13 @@ import lombok.Data;
 @Data
 public class UseClauseBeforeResponseDto {
 
+    private String useClauseKind;
     private String useClauseTitle;
     private String fileUrl;
     private String createDate;
 
     public UseClauseBeforeResponseDto(UseClause useClause) {
+        this.useClauseKind = useClause.getUseClauseKind();
         this.useClauseTitle = useClause.getUseClauseTitle();
         this.fileUrl = useClause.getCommonFile().getFileUrl();
         this.createDate = useClause.getCreatedDate().substring(0, 10);
