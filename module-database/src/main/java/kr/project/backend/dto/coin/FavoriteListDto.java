7098ab394d5285b0a21a1d21e87cca506eda9161
youngjae <newy12@naver.com>
2024-02-10 00:40:29 +0900

    #feat. 스테이킹 메인 목록 조회 캐시 5분추가

--- module-database/src/main/java/kr/project/backend/dto/coin/FavoriteListDto.java
+++ module-database/src/main/java/kr/project/backend/dto/coin/FavoriteListDto.java
@@ -3,15 +3,17 @@ package kr.project.backend.dto.coin;
 import io.swagger.v3.oas.annotations.media.Schema;
 import kr.project.backend.entity.user.Favorite;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class FavoriteListDto {
     @Schema(description = "스테이킹 키값", example = "스테이킹 키값")
     private String stakingId;
     @Schema(description = "코인이름", example = "폴리곤 (MATIC)")
     private String coinName;
-    //@Schema(description = "코인이미지URL", example = "URL")
-    //private String coinImageUrl;
+    @Schema(description = "코인이미지URL", example = "URL")
+    private String coinImageUrl;
     @Schema(description = "연 추정 보상률 (최소)", example = "5.3%")
     private String minAnnualRewardRate;
     @Schema(description = "연 추정 보상률 (최대)", example = "10.3%")
@@ -20,7 +22,7 @@ public class FavoriteListDto {
     public FavoriteListDto(Favorite favorite) {
         this.stakingId = favorite.getStakingInfo().getStakingId();
         this.coinName = favorite.getStakingInfo().getCoinName();
-        //this.coinImageUrl = favorite.getStakingInfo().getCoinImageUrl() == null ? "" : favorite.getStakingInfo().getCoinImageUrl();
+        this.coinImageUrl = favorite.getStakingInfo().getCoinImageUrl() == null ? "" : favorite.getStakingInfo().getCoinImageUrl();
         this.minAnnualRewardRate = favorite.getStakingInfo().getMinAnnualRewardRate();
         this.maxAnnualRewardRate = favorite.getStakingInfo().getMaxAnnualRewardRate();
     }
