f10b1d1ae5d3e33fc3b4546e01f490618329fb0c
youngjae <newy12@naver.com>
2024-02-11 11:02:20 +0900

    #feat. 캐시쪽 코드 리펙토링

--- module-api/src/main/java/kr/project/backend/config/RedisConfig.java
+++ module-api/src/main/java/kr/project/backend/config/RedisConfig.java
@@ -1,17 +1,25 @@
 package kr.project.backend.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
@@ -31,5 +39,35 @@ public class RedisConfig {
         redisStandaloneConfiguration.setPassword(password);
 
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+    //redis 관련 설정
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(300)) //5분
+                .disableCachingNullValues()
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
+                )
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager() {
+        RedisCacheManager redisCacheManager = RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory())
+                .cacheDefaults(redisCacheConfiguration())
+                .withInitialCacheConfigurations(customCacheConfig())
+                .build();
+        redisCacheManager.setTransactionAware(false);
+        return redisCacheManager;
+    }
+    @Bean
+    public Map<String,RedisCacheConfiguration> customCacheConfig() {
+        Map<String,RedisCacheConfiguration> caches = new HashMap<>();
+        caches.put("stakingInfoList",redisCacheConfiguration().entryTtl(Duration.ofSeconds(300))); //5분
+        return caches;
     }
 }
