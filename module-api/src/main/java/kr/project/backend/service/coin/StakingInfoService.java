ae0a5ae0a25faaa018d7984f88edb06e04d6cea9
youngjae <newy12@naver.com>
2024-02-11 10:32:38 +0900

    #feat. 스테이킹 메인 목록 조회 캐시 5분추가

--- module-api/src/main/java/kr/project/backend/service/coin/StakingInfoService.java
+++ module-api/src/main/java/kr/project/backend/service/coin/StakingInfoService.java
@@ -34,7 +34,7 @@ public class StakingInfoService {
     private final FavoriteRepository favoriteRepository;
     private final UserRepository userRepository;
 
-    //@Cacheable(value = "stakingInfoList")
+    @Cacheable(value = "stakingInfoList",key = "#serviceUser.userId.concat('-').concat(#serviceUser.userEmail)")
     public StakingInfoAndFavoriteListResponseDto getStakingInfosAll(ServiceUser serviceUser) {
         //날짜 조건식
         LocalDate today = LocalDate.now();
