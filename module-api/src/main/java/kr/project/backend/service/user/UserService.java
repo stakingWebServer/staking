6bb27049fd57adde2caf591d3cdb092a113cebd7
youngjae <newy12@naver.com>
2024-02-11 12:14:42 +0900

    #feat. 즐겨찾기 시, 캐시 초기화

--- module-api/src/main/java/kr/project/backend/service/user/UserService.java
+++ module-api/src/main/java/kr/project/backend/service/user/UserService.java
@@ -23,6 +23,7 @@ import kr.project.backend.repository.user.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -277,6 +278,7 @@ public class UserService {
     }
 
     @Transactional
+    @CacheEvict(value = "stakingInfoList",allEntries = true)
     public void addFavorite(ServiceUser serviceUser, AddFavoriteRequestDto addFavoriteRequestDto) {
         //회원정보
         User userInfo = userRepository.findById(serviceUser.getUserId())
@@ -294,6 +296,7 @@ public class UserService {
     }
 
     @Transactional
+    @CacheEvict(value = "stakingInfoList",allEntries = true)
     public void unFavorite(ServiceUser serviceUser, UnFavoriteRequestDto unFavoriteRequestDto) {
         //회원정보
         User userInfo = userRepository.findById(serviceUser.getUserId())
